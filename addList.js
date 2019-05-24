29f22d47109c0bcb740daf7ab05cedaba63d5ce0
David510V <rt56223@gmail.com>
2019-05-24 18:58:48 +0300

    addPrice.js

--- addList.js
+++ addList.js
@@ -1,2 +1,42 @@
+
+function addList() {
+
   sadsa
+
+  asdss
 var li =document.createElement("li");
+var myAdd = document.getElementById("myAdd").value;
+
+var te = document.createTextNode(myAdd);
+
+li.appendChild(te);
+
+    if (myAdd === '' ) 
+   {
+     Swal.fire ('Hey' , 'You didnt do anything today ?' , 'error') ;
+   }
+
+   else 
+   {
+      document.getElementById("myUrl").appendChild(li);
+   }
+sada
+   document.getElementById("myAdd").value= "";
+
+   var span = document.createElement("SPAN");
+   var txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   li.appendChild(span);
+ 
+   for (i = 0; i < close.length; i++) 
+   {
+     close[i].onclick = function()
+     {
+       var div = this.parentElement;
+       div.style.display = "none";
+     }
+
+   }  
+
+}
